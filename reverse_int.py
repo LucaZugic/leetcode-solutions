0f0e6b3838cf0f07113591b7a25394fc96e284ef
Luca Zugic <62926784+LucaZugic@users.noreply.github.com>
2021-01-14 08:23:02 +0000

    Create reverse_int.py

---
+++ reverse_int.py
@@ -0,0 +1,18 @@
+# using string
+class Solution:
+    def reverse(self, x: int) -> int:
+        s = str(x)
+        if '-' in s:
+            s = s.replace("-","")
+        ans = [None]*len(s)
+        i = len(s)
+        for j in map(int, s):
+            ans[i-1] = j
+            i-=1
+        ans = int(''.join(str(i) for i in ans))
+        if ans > 2**31:
+            return 0
+        else:
+            if x < 0:
+                ans = 0 - ans
+            return ans
